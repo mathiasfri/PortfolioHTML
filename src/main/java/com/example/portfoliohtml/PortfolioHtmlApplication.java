3cf34413cc13827e860542917bf6306c0f45d28b
mathiasfri <mathiasfridthjof@live.dk>
2023-02-10 11:13:05 +0100

    Everything

---
+++ src/main/java/com/example/portfoliohtml/PortfolioHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfoliohtml/PortfolioHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
